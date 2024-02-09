891a2a04ffcd481dabc668d6462e0e65a6cfd4a1
Lee Mander <lee.mander@gmail.com>
2024-02-09 12:00:35 +0000

    Style destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Destination({ destinations }) {
   const [currentDestIndex, setCurrentDestIndex] = useState(0);
   return (
-    <main>
+    <main className="destination">
       <div className="container">
         <h1>
           <span>01</span> Pick Your Destination
@@ -12,14 +12,23 @@ export default function Destination({ destinations }) {
           <img src={destinations[currentDestIndex].images.webp} alt={""} />
           <div className="destination__content">
             <div className="destination__controls">
-              <button onClick={() => setCurrentDestIndex(0)}>Moon</button>
+              <button
+                className="current"
+                onClick={() => setCurrentDestIndex(0)}
+              >
+                Moon
+              </button>
               <button onClick={() => setCurrentDestIndex(1)}>Mars</button>
               <button onClick={() => setCurrentDestIndex(2)}>Europa</button>
               <button onClick={() => setCurrentDestIndex(3)}>Titan</button>
             </div>
             <div>
-              <h2>{destinations[currentDestIndex].name}</h2>
-              <p>{destinations[currentDestIndex].description}</p>
+              <h2 className="destination__name">
+                {destinations[currentDestIndex].name}
+              </h2>
+              <p className="destination__desc">
+                {destinations[currentDestIndex].description}
+              </p>
               <hr />
               <div className="two-column">
                 <div>

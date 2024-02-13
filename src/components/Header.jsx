5e69a6420ab69564956e96647e5b42b40051693b
Lee Mander <lee.mander@gmail.com>
2024-02-13 10:53:41 +0000

    Add mobile navigation menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const currentPage = useLocation().pathname;
+  const [menuOpen, setMenuOpen] = useState(false);
+  function toggleMenu() {
+    setMenuOpen((prev) => !prev);
+  }
   return (
     <header>
       <div className="container">
         <div className="space-between">
           <img src="logo.svg" alt="" />
-          <nav className="header__nav">
+          <button
+            aria-label="toggle navigation menu"
+            className="header__hamburger"
+            onClick={toggleMenu}
+          ></button>
+          <nav className={menuOpen ? "header__nav open" : "header__nav"}>
             <ul>
               <li>
                 <Link
